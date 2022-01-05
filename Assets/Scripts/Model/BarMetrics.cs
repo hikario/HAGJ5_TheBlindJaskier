41156a3666b0c71210fbbcfd8bbfc09447d59702
Igor Oreshkin <Igor.d.oreshkin@gmail.com>
2022-01-05 01:52:57 -0800

    created some modal

---
+++ Assets/Scripts/Model/BarMetrics.cs
@@ -0,0 +1,48 @@
+﻿namespace Assets.Scripts.Model
+{
+    public static class GlobalBar
+    {
+        // global storage for our bar, could be only one instance
+                        
+        [System.Flags]
+        public enum AlcoholSources
+        {
+            Detroit = 1 << 0,
+            Moonshine = 1 << 1,
+            Mafia = 1 << 2,
+        }
+
+        /// <summary>
+        /// bitvise mask of available sources, all 3 sources = 7 ( 1 | 2 | 4 )
+        /// </summary>
+        public static AlcoholSources SourceAvailability { get; set; }
+        public static AlcoholSources CurrentSource { get; set; }
+        public static AlcoholQualityes CurrentQuality { get; set; }
+        public static AlcoholPrices CurrentAlcoholPrices { get; set; }
+        public static decimal Money { get; set; }
+        public static int SatisfiedImportantCustomers { get; set; }
+        public static uint Popularity { get; set; }
+        public static int RaidProbability { get; set; }
+        public static int RaidProbabilityStep { get; set; }
+
+        static GlobalBar()
+        {
+            foreach (AlcoholSources t in System.Enum.GetValues(typeof(AlcoholSources)))
+                SourceAvailability |= t;
+
+            RaidProbabilityStep = 5; // could come from some settings
+        }
+        
+        public static bool IsSourceAvailable (AlcoholSources alcoholSource)
+        {
+            return SourceAvailability.HasFlag(alcoholSource);
+        }
+
+        public static void IncreaseRaidProbability(int stepsForIncrease = 1)
+        {
+            RaidProbability += RaidProbabilityStep * stepsForIncrease;
+            if (RaidProbability < 0)
+                RaidProbability = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Model/CopCustomer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Model
+{
+    public class CopCustomer : BaseCustomer
+    {
+        //Cops:
+        //    - randomly know or don't know the password
+        //    - never on the list
+        //    - 1/20 chance if they're allowed in they demand bribe
+        //    - randomly determine quality preference (Igor: assume they have better income and livestyle than workers from moving companies..)
+        //    - randomly determine price preference
+        //    - if expectations unmet, increase probability of raid
+        //    - if expectations met, lower probability of raid
+        //    -* if expectations surpassed, greatly lower probability of raid
+        //    - will not be repeat customer, period   (Igor: Why not ? it could be fun to have full bar of cops and get raid right after price increase ;) 
+
+        public bool IsDemandBribe { get; set; }
+        public CopCustomer()
+        {
+            base.IsOnList = false;
+            base.QualityPreference = (AlcoholQualityes)Random.Range(1, 3); // Igor: assume they have better income and livestyle than workers from moving companies..
+            base.PricePreference = (AlcoholPrices)Random.Range(1, 3); // it could be med-high instead
+            IsDemandBribe = Random.Range(1, 20) == 1; // 1/20 chance if they're allowed in they demand bribe
+        }
+
+        override public int ProcessExpectation(AlcoholQualityes currentQuality, AlcoholPrices currentPrice)
+        {
+            var expectationMatch = CheckExpectation(currentQuality, currentPrice);
+
+            if (expectationMatch < 0)
+                callTheCops();
+            else if (expectationMatch > 0)
+                guardFromNextRaid(expectationMatch);
+
+            return expectationMatch;
+        }
+        private void callTheCops()
+        {
+            // event for anymation..
+            GlobalBar.IncreaseRaidProbability(1);
+        }
+        private void guardFromNextRaid(int enjoyLvl)
+        {
+            // event for anymation..
+            GlobalBar.IncreaseRaidProbability(enjoyLvl * -1);
+        }
+    }
+}
