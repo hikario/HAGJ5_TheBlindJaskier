b934c459ba78dac6a117b6f84d078b169a0b618f
Igor Oreshkin <Igor.d.oreshkin@gmail.com>
2022-01-05 01:52:57 -0800

    created some modal

---
+++ Assets/Scripts/Model/BaseCustomer.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Model
+{
+    public enum PassEnum
+    {
+        NoPassword,
+        HasNewPassword,
+        HasOldPassword,
+    }
+    public enum AlcoholQualityes
+    {
+        None,
+        Low,
+        Medium,
+        High,
+    }
+
+    public enum AlcoholPrices
+    {
+        None,
+        Low,
+        Medium,
+        High,
+    }
+
+    /// <summary>
+    /// Represents customer in general ("Regular customers" and "Repeat Customers")
+    /// </summary>
+    public class BaseCustomer 
+    {
+        //Regular customers:
+        //    - randomly know or don't know the password
+        //    - randomly on or not on the list
+        //    - randomly determine quality preference
+        //    - randomly determine price preference
+        //    - if expectations unmet, popularity down
+        //    - if expectations met, become repeat customers and increase popularity,
+        //      and increase price and quality preference range, buy extra drink
+        //        - if expectations exceeded in one, buy 3 drinks total
+        //    - if expectations met or exceeded, they become repeat customers
+        //
+        //Repeat Customers:
+        //    - know password
+        //    - on list
+        //    - quality preference extended down by one
+        //    - price preference extended up by one
+        //    - if expectations met, popularity up, buy two extra drinks
+        //    - if expectations exceeded, buy four extra drinks
+        //    - if expectations unmet, lose expanded preference
+        //        - if expectations unmet twice, they lose repeat status
+        int _fanLevel = 0;
+
+        public PassEnum Password { get; set; }
+        public AlcoholQualityes QualityPreference { get; set; }
+        public AlcoholPrices PricePreference { get; set; }
+        public AlcoholQualityes QualityPreferenceOriginal { get; }
+        public AlcoholPrices PricePreferenceOriginal { get; }
+        public bool IsOnList { get; protected set; } = false;
+        
+        public BaseCustomer() 
+        {
+            Password = (PassEnum)Random.Range(0, 1);
+            QualityPreference = QualityPreferenceOriginal = (AlcoholQualityes)Random.Range(0, 3);
+            PricePreference = PricePreferenceOriginal = (AlcoholPrices)Random.Range(0, 3);
+        }
+
+        virtual public int ProcessExpectation(AlcoholQualityes currentQuality, AlcoholPrices currentPrice)
+        {
+            // not sure if we want to do trade offs, like if cutomer want to pay more money if quality is more than he expected.
+            var expectationMatch = CheckExpectation(currentQuality, currentPrice);
+
+            if (expectationMatch < 0)
+                --FanLevel;
+
+            //- if expectations met, become repeat customers and increase popularity,
+            if (expectationMatch >= 0)
+            {
+                if (IsOnList == false) IsOnList = true;
+                ++FanLevel; //and increase popularity
+            }
+            
+            //if expectations exceeded in one, buy 3 drinks total
+            int i = expectationMatch;
+            if (i > 3) i = 3;
+            while (--i > 0)
+                BuyExtraDrink();
+
+            return expectationMatch;
+        }
+
+        /// <summary>
+        /// return -1 if expectetion unment, 0 - if met exactly, >0 - if expectation exceeded
+        /// </summary>
+        /// <param name="currentQuality"></param>
+        /// <param name="currentPrice"></param>
+        /// <returns></returns>
+        protected int CheckExpectation(AlcoholQualityes currentQuality, AlcoholPrices currentPrice)
+        {
+            // not sure if we want to do trade offs, like if cutomer want to pay more money if quality is more than he expected.
+            if (currentQuality < this.QualityPreference || currentPrice > this.PricePreference)
+                return -1;
+
+            int extraexpectations = 0;
+            if (currentQuality > this.QualityPreference)
+                ++extraexpectations;
+
+            if (currentPrice < this.PricePreference)
+                ++extraexpectations;
+
+            return extraexpectations;
+        }
+
+        public int FanLevel
+        {
+            get => _fanLevel;
+            set
+            {
+                if (value > _fanLevel && _fanLevel > 0 ) // it is already positive..
+                    return;
+
+                if (value < _fanLevel && _fanLevel < 0) // it is already negative..
+                    return;
+
+                _fanLevel = value;
+                if (_fanLevel == 0)
+                {
+                    QualityPreference = QualityPreferenceOriginal;
+                    PricePreference = PricePreferenceOriginal;
+                }
+                else if (_fanLevel > 0)
+                {
+                    // and increase price and quality preference range
+
+                    if (QualityPreference != AlcoholQualityes.None)
+                        --QualityPreference;
+
+                    if (PricePreference != AlcoholPrices.High)
+                        ++PricePreference;
+                }
+                //else  if _fanLevel < 0 customer will not come back
+
+
+            }
+        }
+
+        protected void BuyExtraDrink()
+        {
+            // event for anymation..
+
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Model/ImportantCustomer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Model
+{
+    /// <summary>
+    /// Represents "Important customers"
+    /// </summary>
+    public class ImportantCustomer : BaseCustomer
+    {
+        //    mportant customers:
+        //    - randomly know or don't know the password
+        //    - always on the list
+        //    - always want high quality
+        //    - always have full price preference
+        //    - if quality is middle, no consequence(just an unsatisfied customer)
+        //    - if quality is low, increase the number of cops for the following year
+        //    - if expectations met, will protect from one raid
+        //    - will not be a repeat customer, period
+
+        public ImportantCustomer()
+        {
+            base.IsOnList = true; ////always on the list
+            base.QualityPreference = AlcoholQualityes.High; //always want high quality
+            base.PricePreference = AlcoholPrices.High; //always have full price preference
+        }
+        override public int ProcessExpectation(AlcoholQualityes currentQuality, AlcoholPrices currentPrice)
+        {
+            var expectationMatch = CheckExpectation(currentQuality, currentPrice);
+
+            if (expectationMatch < 0)
+                callTheCops();
+            else if (expectationMatch > 0)
+                guardFromNextRaid();
+
+            return expectationMatch;
+        }
+        private void callTheCops()
+        {
+            // event for anymation..
+
+            GlobalBar.IncreaseRaidProbability(1);
+        }
+        private void guardFromNextRaid()
+        {
+            // event for anymation..
+
+            ++GlobalBar.SatisfiedImportantCustomers;
+        }
+    }
+}
