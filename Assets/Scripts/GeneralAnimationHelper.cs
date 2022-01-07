f656d26d494450eec5256e18a22d2cc644b2ca81
Ramfa <Igor.d.oreshkin@gmail.com>
2022-01-06 23:28:54 -0800

    add my scene and play with animations

---
+++ Assets/Scripts/GeneralAnimationHelper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralAnimationHelper : MonoBehaviour
+{
+    public AnimationClip Entry;
+    public AnimationClip Accepted;
+    public AnimationClip Rejected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
