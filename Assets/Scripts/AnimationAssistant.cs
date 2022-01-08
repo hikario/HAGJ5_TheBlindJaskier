aaa44d5a5dcbbc211a30ac84da5e4622d0062169
Cesar Polanco <inann231@gmail.com>
2022-01-08 06:45:00 -0800

    Tighten up bottle animations

--- Assets/Scripts/AnimationAssistant.cs
+++ Assets/Scripts/AnimationAssistant.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class AnimationAssistant : MonoBehaviour
 {
     private Animation anim;
+    private bool pulledSwitch;
 
     void Awake()
     {
         anim = gameObject.GetComponent<Animation>();
+        pulledSwitch = false;
 
-        EventManager.RegisterEventListener("PanicButtonPressed", OnPanic);        
+        EventManager.RegisterEventListener("PanicButtonPressed", OnPanic);
+        EventManager.RegisterEventListener("EndOfTheNight", ResetFurniture);     
     }
 
     void OnDestroy()
     {
         EventManager.DeregisterEventListener("PanicButtonPressed", OnPanic);
+        EventManager.DeregisterEventListener("EndOfTheNight", ResetFurniture);     
     }
 
     void OnPanic()
     {
+        pulledSwitch = true;
         StartCoroutine(PopDrawer());
+    }
+
+    void ResetFurniture()
+    {
+        if(pulledSwitch)
+        {
+            StartCoroutine(WithdrawFromDrawer());
+        }
     }
 
     IEnumerator PopDrawer()
@@ -28,6 +41,14 @@ public class AnimationAssistant : MonoBehaviour
         anim.Play("AN_BarOpen");
         yield return new WaitForSeconds(1);
         anim.Play("AN_BarClose");
+    }
+
+    IEnumerator WithdrawFromDrawer()
+    {
+        anim.Play("AN_BarOpen");
+        yield return new WaitForSeconds(1.25f);
+        anim.Play("AN_BarClose");
+        pulledSwitch = false;
     }
 
 }
--- Assets/Scripts/BottleAnimationAssistant.cs
+++ Assets/Scripts/BottleAnimationAssistant.cs
@@ -5,27 +5,47 @@ using UnityEngine;
 public class BottleAnimationAssistant : MonoBehaviour
 {
     private Animation anim;
+    private bool pulledSwitch;
 
     void Awake()
     {
         anim = gameObject.GetComponent<Animation>();
+        pulledSwitch = false;
 
-        EventManager.RegisterEventListener("PanicButtonPressed", OnPanic);        
+        EventManager.RegisterEventListener("PanicButtonPressed", OnPanic);
+        EventManager.RegisterEventListener("EndOfTheNight", ResetFurniture);             
     }
 
     void OnDestroy()
     {
         EventManager.DeregisterEventListener("PanicButtonPressed", OnPanic);
+        EventManager.DeregisterEventListener("EndOfTheNight", ResetFurniture);     
     }
 
     void OnPanic()
     {
+        pulledSwitch = true;
         StartCoroutine(DropBottle());
+    }
+
+    void ResetFurniture()
+    {
+        if(pulledSwitch)
+        {
+            StartCoroutine(RaiseBottle());
+        }
     }
 
     IEnumerator DropBottle()
     {
         yield return new WaitForSeconds(0.25f);
         anim.Play("AN_BottleDrop");
+    }
+
+    IEnumerator RaiseBottle()
+    {
+        yield return new WaitForSeconds(0.25f);
+        anim.Play("AN_BottleLift");
+        pulledSwitch = false;
     }
 }
