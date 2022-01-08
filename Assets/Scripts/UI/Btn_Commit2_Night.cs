30de94194d817af4f00e4909223fb53a3a3f46a1
Ramfa <Igor.d.oreshkin@gmail.com>
2022-01-08 00:10:43 -0800

    wait for event "TheNightEnds" for the night phase

---
+++ Assets/Scripts/UI/Btn_Commit2_Night.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class Btn_Commit2_Night : MonoBehaviour
+{
+    GameFlow _gameFlowObject;
+    Button _btn;
+
+
+    private void Awake()
+    {
+        var gameObject = GameObject.Find("/GameManager");
+        _gameFlowObject = gameObject.GetComponent<GameFlow>();
+        
+        _btn = GetComponent<Button>();
+        _btn.onClick.AddListener(OnClick);
+        EventManager.RegisterEventListener("TheNightEnds", OnATheNightEnds);
+    }
+    private void Update()
+    {
+
+    }
+    void OnDestroy()
+    {
+        EventManager.DeregisterEventListener("TheNightEnds", OnATheNightEnds);
+    }
+    void OnEnable()
+    {
+        _btn.interactable = false;
+    }
+    void OnATheNightEnds()
+    {
+        _btn.interactable = true;
+    }
+
+
+    void OnClick()
+    {
+        _gameFlowObject.ProgressDay();
+    }
+}
+
+
