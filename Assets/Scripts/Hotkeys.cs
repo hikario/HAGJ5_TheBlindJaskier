d0b1195eafdce0594fd1c34d73c915bdaeec9f73
Ramfa <Igor.d.oreshkin@gmail.com>
2022-01-08 06:52:25 -0800

    disabled debugging Hotkeys

--- Assets/Scripts/Hotkeys.cs
+++ Assets/Scripts/Hotkeys.cs
@@ -15,31 +15,28 @@ public class Hotkeys : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F12))
-        {
-            Debug.Log("Hotkey Pressed = F12");
-            //if (TestGenerator != null)
-            //    TestCharacter = TestGenerator.SpawnZomby();
-            //GlobalBar.SourceAvailability ^= GlobalBar.AlcoholSources.Moonshine | GlobalBar.AlcoholSources.Mafia;
-            GlobalBar.SourceAvailability ^= GlobalBar.AlcoholSources.Detroit;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F9))
-        {
-            if (TestCharacter != null)
-                TestCharacter.Emotion_Happy();
-        }
-
-        if (Input.GetKeyDown(KeyCode.F10))
-        {
-            if (TestCharacter != null)
-                TestCharacter.Emotion_Neutral();
-        }
-        if (Input.GetKeyDown(KeyCode.F11))
-        {
-            if (TestCharacter != null)
-                TestCharacter.Emotion_Sad();
-        }
+        //if (Input.GetKeyDown(KeyCode.F12))
+        //{
+        //    Debug.Log("Hotkey Pressed = F12");
+        //    //if (TestGenerator != null)
+        //    //    TestCharacter = TestGenerator.SpawnZomby();
+        //    //GlobalBar.SourceAvailability ^= GlobalBar.AlcoholSources.Moonshine | GlobalBar.AlcoholSources.Mafia;
+        //    GlobalBar.SourceAvailability ^= GlobalBar.AlcoholSources.Detroit;
+        //}
+        //
+        //if (Input.GetKeyDown(KeyCode.F9))
+        //{
+        //    EventManager.TriggerEvent("DropBottels");
+        //}
+        //
+        //if (Input.GetKeyDown(KeyCode.F10))
+        //{
+        //    EventManager.TriggerEvent("CallPolice"); 
+        //}
+        //if (Input.GetKeyDown(KeyCode.F11))
+        //{
+        //    EventManager.TriggerEvent("PoliceGoDown");
+        //}
 
     }
 }
