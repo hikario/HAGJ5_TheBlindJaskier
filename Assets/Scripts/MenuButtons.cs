e0ce8d2648154db7b15117a80025f55af629128d
hikario <58058800+hikario@users.noreply.github.com>
2022-01-08 04:07:43 -0800

    Quit quits out of playmode

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
     public void QuitGame()
     {
         Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     public void SetFullscreen (bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void StartOver()
+    {
+        SceneManager.LoadScene( SceneManager.GetActiveScene().name );
     }
 }
